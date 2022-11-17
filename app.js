5a4cbde290e22c900a8bc480dc1ca9559ca6c324
Mustafa-ali123 <111171988+Mustafa-ali123@users.noreply.github.com>
2022-11-17 12:30:04 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+function change(){
+var img = document.getElementById("img")
+img.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyZQCOMJmPZf3i_JsgichtdB4cDJzgIGPFBQ&usqp=CAU"
+img.setAttribute('height','600pc')
+}
+
+function change2(){
+    var img = document.getElementById("img")
+    img.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxj9ygkht0x3QJNQBioIFTUNHvgekFtNfMjKOO96Dm-5DPZHEy0dMRu-LUK9Mjij4JTWw&usqp=CAU"
+    
+    }
+
+
+    
+
+    
